b00f8d70e6a3f3d64d08ecf7453b53a99ed9fd79
itsmethemojo <marianpoeschmann@googlemail.com>
2020-10-22 02:39:28 +0200

    add build target for heroku

--- .dckrz/scripts/build.sh
+++ .dckrz/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 
-curl -sL https://raw.githubusercontent.com/luisgagocasas/heroku-php-nginx-simple/master/nginx_app.conf -o nginx_app.conf
+curl -sL https://raw.githubusercontent.com/luisgagocasas/heroku-php-nginx-simple/master/nginx_app.conf -o nginx_app.conf && \
+curl -sL https://raw.githubusercontent.com/luisgagocasas/heroku-php-nginx-simple/master/composer.json -o composer.json && \
+curl -sL https://raw.githubusercontent.com/luisgagocasas/heroku-php-nginx-simple/master/composer.lock -o composer.lock && \
 .dckrz/scripts/build-dev.sh
